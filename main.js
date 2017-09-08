8152f26ef599de774bd8280f9f45c7e104fdf5ea
Gordon Doskas <gordon2012@users.noreply.github.com>
2017-09-07 18:43:25 -0700

    add live reloading
    Signed-off-by: Gordon Doskas <gordon2012@users.noreply.github.com>

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
 'use strict';
 
 var electron = require('electron');
-
 var app = electron.app;
 var BrowserWindow = electron.BrowserWindow;
-
 var mainWindow = null;
+
+var path = require('path');
+var reload = require('electron-reload');
+var isDev = require('electron-is-dev');
+
+if(isDev) {
+    const electronPath = path.join(__dirname, 'node_modules', '.bin', 'electron');
+    reload(__dirname, {electron: electronPath});
+}
 
 app.on('ready', function() {
     mainWindow = new BrowserWindow({
